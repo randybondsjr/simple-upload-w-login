6463ed7f491abe8d894c4a8791465e0f00897a37
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-03-17 09:41:19 -0700

    Updated login script to mysqli oop

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
   start_session();
-  //http://css-tricks.com/snippets/php/login-function/
+  
   // function to escape data and strip tags
   function safestrip($string){
          $string = strip_tags($string);
-         $string = mysql_real_escape_string($string);
+         $string = filter_var($string, FILTER_SANITIZE_STRING);
          return $string;
   }
   
@@ -24,33 +24,42 @@
   
   // log user in function
   function login($username, $password){
-  
+    //login error
+    $login_error = "Sorry, wrong username or password";
+    
     //call safestrip function
     $user = safestrip($username);
     $pass = safestrip($password);
     
     //convert password to md5
     $pass = md5($pass);
+    //connect to the db using predefined function
+    $mysqli = ConnectToDBo('db');
     
     // check if the user id and password combination exist in database
-    $sql = mysql_query("SELECT * FROM table WHERE username = '$user' AND password = '$pass'")or die(mysql_error());
-    
-    //if match is equal to 1 there is a match
-    if (mysql_num_rows($sql) == 1) {
-    
-      //set session
-      $_SESSION['authorized'] = true;
-      
-      // reload the page
-      $_SESSION['success'] = 'Login Successful';
-      header('Location: ./index.php');
-      exit;
-    
-    
-    } else {
+    $sql = "SELECT * FROM table WHERE username = '$user' AND password = '$pass' LIMIT 0,1";
+    //if things worked
+    if ($result = $mysqli->query($sql)) {
+    //we need to check for results
+    $row_cnt = $result->num_rows;
+       //if match is equal to 1 there is a match
+      if($row_cnt == 1){
+        //set session
+        $_SESSION['authorized'] = true;
+        
+        // reload the page
+        $_SESSION['success'] = 'Login Successful';
+        header('Location: ./index.php');
+        exit;
+      }else{
+        //no results need an error
+        $_SESSION['error'] = $login_error;
+      }
+      //clean up the result set
+      $result->free();
+    }else{
       // login failed save error to a session
-      $_SESSION['error'] = 'Sorry, wrong username or password';
+      $_SESSION['error'] = $login_error;
     }
   }
-  //END css-tricks.com snippet
 ?>
