f7a6c84e7f0caed47a46854126ead38ef3549cb5
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-03-17 10:47:12 -0700

    Added HTML

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+<!doctype html>
 <?php 
   session_start();
-  
+  require("../includes/functions.php");
   // function to escape data and strip tags
   function safestrip($string){
          $string = strip_tags($string);
@@ -34,7 +35,7 @@
     //convert password to md5
     $pass = md5($pass);
     //connect to the db using predefined function
-    $mysqli = ConnectToDBo('db');
+    $mysqli = ConnectToDBo('wptransit');
     
     // check if the user id and password combination exist in database
     $sql = "SELECT * FROM table WHERE username = '$user' AND password = '$pass' LIMIT 0,1";
@@ -62,4 +63,31 @@
       $_SESSION['error'] = $login_error;
     }
   }
+
+  print_r($_SESSION);
+  
 ?>
+<html>
+<head>
+  <title>Upload</title>
+</head>
+<body>
+  <form role="form">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputFile">File input</label>
+      <input type="file" id="exampleInputFile">
+      <p class="help-block">Example block-level help text here.</p>
+    </div>
+  
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</body>
+</html>
