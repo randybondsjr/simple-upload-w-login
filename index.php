717b008ffb3f7bd1e48da721d7ec08bd5fbdcca8
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-03-17 11:49:51 -0700

    Added logic to check if user if logged in, also added upload file form

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
   session_start();
   require("../includes/functions.php");
   require("./includes/functions.php");
-
   
+  //login to the page
   if(isset($_POST["username"]) && $_POST["username"] != ''){
     $username = filter_var($_POST["username"], FILTER_SANITIZE_STRING);
     $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
     login($username, $password);
   }
-  print_r($_SESSION);
 ?>
 <!doctype html>
 <html>
@@ -17,6 +16,7 @@
   <title>Upload</title>
 </head>
 <body>
+<?php if($_SESSION["authorized"] != 1): //check if user is logged in ?>
   <form role="form" method="POST" action="./">
     <div class="form-group">
       <label for="username">Username</label>
@@ -28,5 +28,16 @@
     </div>
     <button type="submit" class="btn btn-default">Submit</button>
   </form>
+<?php else: ?>
+  <p>Use this form to upload a .zip GTFS file</p>
+  <form role="form" method="POST" action="./" enctype="multipart/form-data">
+    <div class="form-group">
+      <label for="gtfsFile">GTFS File</label>
+      <input type="file" name="gtfsFile" id="gtfsFile">
+      <p class="help-block">Please upload a GTFS .zip file only. All other files will result in an error.</p>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+<?php endif; ?>
 </body>
 </html>
