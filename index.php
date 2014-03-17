be7d36cc3e701050ea0b11d5263e967cfcfef527
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-03-17 09:29:17 -0700

    fixed spacing

--- index.php
+++ index.php
@@ -25,31 +25,31 @@
   // log user in function
   function login($username, $password){
   
-   //call safestrip function
-   $user = safestrip($username);
-   $pass = safestrip($password);
-  
-   //convert password to md5
-   $pass = md5($pass);
-  
+    //call safestrip function
+    $user = safestrip($username);
+    $pass = safestrip($password);
+    
+    //convert password to md5
+    $pass = md5($pass);
+    
     // check if the user id and password combination exist in database
     $sql = mysql_query("SELECT * FROM table WHERE username = '$user' AND password = '$pass'")or die(mysql_error());
-  
+    
     //if match is equal to 1 there is a match
     if (mysql_num_rows($sql) == 1) {
-  
-                            //set session
-                            $_SESSION['authorized'] = true;
-  
-                            // reload the page
-                           $_SESSION['success'] = 'Login Successful';
-                           header('Location: ./index.php');
-                           exit;
-  
-  
-     } else {
-                 // login failed save error to a session
-                 $_SESSION['error'] = 'Sorry, wrong username or password';
+    
+      //set session
+      $_SESSION['authorized'] = true;
+      
+      // reload the page
+      $_SESSION['success'] = 'Login Successful';
+      header('Location: ./index.php');
+      exit;
+    
+    
+    } else {
+      // login failed save error to a session
+      $_SESSION['error'] = 'Sorry, wrong username or password';
     }
   }
   //END css-tricks.com snippet
